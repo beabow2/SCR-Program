7a8b106538b38c404eadfaf8cd429b3be3b39b7f
Han Pao <115783837+beabow2@users.noreply.github.com>
2023-08-28 16:26:33 +0800

    Add files via upload
    Make GUI analysis function parameter into classes

--- parameter.py
+++ parameter.py
@@ -8,6 +8,7 @@ DEFAULT_TARGET = 1
 class Parameters:
     def __init__(self):
         """
+        Initialize parameters with default values.\n
         Rise Start-Begin: the closest point in time following an event at which a potential response can start in
         order for it to be considered valid (default 0.5 sec.)\n
         Rise Start-End: the furthest point in time following an event at which a potential response can start in order
@@ -16,10 +17,36 @@ class Parameters:
         valid (default 5 sec.)\n
         Display_window: the length of time following an event in which the program will look for responses in the
         electrodermal data (default 10 sec.)\n
-        Target: the reversed number of event (default 1.)
+        Target: the reversed number of event (default 1)
+        """
+        self.target = None
+        self.display_window = None
+        self.max_rise_time = None
+        self.rise_end = None
+        self.rise_begin = None
+        self.reset_to_defaults()
+
+    def reset_to_defaults(self):
+        """
+        Reset all parameters to their default values.
         """
         self.rise_begin = DEFAULT_RISE_BEGIN
         self.rise_end = DEFAULT_RISE_END
         self.max_rise_time = DEFAULT_MAX_RISE_TIME
         self.display_window = DEFAULT_DISPLAY_WINDOW
         self.target = DEFAULT_TARGET
+
+    def update_parameters(self, rise_begin=None, rise_end=None, max_rise_time=None, display_window=None, target=None):
+        """
+        Update parameters. If a parameter is None, it will not be updated.
+        """
+        if rise_begin is not None:
+            self.rise_begin = rise_begin
+        if rise_end is not None:
+            self.rise_end = rise_end
+        if max_rise_time is not None:
+            self.max_rise_time = max_rise_time
+        if display_window is not None:
+            self.display_window = display_window
+        if target is not None:
+            self.target = target
